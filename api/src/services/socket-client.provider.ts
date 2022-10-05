412d4b264157dc306ba783f7b78d88820e70e27a
cloakdkilla <getrektwithme1@gmail.com>
2022-10-05 15:16:29 +0100

    remove logging

--- api/src/services/socket-client.provider.ts
+++ api/src/services/socket-client.provider.ts
@@ -61,7 +61,6 @@ export async function handleNewBlock(block: IBsBlock, parseBlockFn: T_ParseBlock
 	for (let sb of subblocks) {
 		const { transactions } = sb;
 		for (let t of transactions) {
-			log.log({ t })
 			const { state, hash, transaction } = t;
 			const fn = transaction.payload.function;
 			const { contract } = transaction.payload;
