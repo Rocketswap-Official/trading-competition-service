532714cbd0dfb6cb3f1c3c187768f041bbe1f9ba
cloakdkilla <getrektwithme1@gmail.com>
2022-10-07 16:09:37 +0100

    remove comments

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,15 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // app.use((req, res, next) => {
-	// 	res.header("Access-Control-Allow-Origin", "rocketswap.exchange"); // update to match the domain you will make the request from
-	// 	res.header("Access-Control-Allow-Origin", "localhost:2001"); // update to match the domain you will make the request from
-	// 	res.header("Access-Control-Allow-Origin", "0.0.0.0:2001"); // update to match the domain you will make the request from
-	// 	res.header("Access-Control-Allow-Origin", "rswp.io"); // update to match the domain you will make the request from
-	// 	res.header("Access-Control-Allow-Origin", "lamden.io"); // update to match the domain you will make the request from
-	// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	// 	next();
-	// });
   app.enableCors()
 
   await app.listen(2001);
